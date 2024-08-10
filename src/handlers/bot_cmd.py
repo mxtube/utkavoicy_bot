002d839fd62a544b473f252ef0f6f20b5c4d5186
Kirill Kuznetsov <kafomin@yandex.ru>
2024-08-10 21:39:47 +0300

    refactor(utka-00017): change method add or update user

--- src/handlers/bot_cmd.py
+++ src/handlers/bot_cmd.py
@@ -1,7 +1,6 @@
 from loguru import logger
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.future import select
 from models.user import User
 
 from aiogram import Router, types
@@ -15,27 +14,7 @@ command_router = Router(name='Bot commands')
 async def start(message: types.Message, session: AsyncSession):
     logger.info(f'User {message.from_user.username} send command {message.text}')
     answer_message = f'Привет, {message.from_user.first_name} {message.from_user.last_name}\nПолучается это бот 🦆'
-
-    result = await session.execute(select(User).filter_by(tg_id=message.from_user.id))
-    user = result.scalars().first()
-
-    if user:
-        user.username = message.from_user.username
-        user.first_name = message.from_user.first_name
-        user.last_name = message.from_user.last_name
-        await session.commit()
-        logger.info(f'User {user.username} was updated in the database')
-    else:
-        new_user = User(
-            username=message.from_user.username,
-            tg_id=message.from_user.id,
-            first_name=message.from_user.first_name,
-            last_name=message.from_user.last_name
-        )
-        session.add(new_user)
-        await session.commit()
-        logger.info(f'User {new_user.username} add in the database')
-
+    await User.create_or_update(session, message.from_user.__dict__)
     await message.answer(answer_message)
     logger.info(f'Bot send to user message: {answer_message}')
 
