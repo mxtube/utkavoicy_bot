837b1a94844604cb1c64a732385939c229858f20
Kirill Kuznetsov <kafomin@yandex.ru>
2024-08-10 16:12:57 +0300

    feat(utka-00012): the start cmd implements adding to db creating and updating user data

--- src/handlers/bot_cmd.py
+++ src/handlers/bot_cmd.py
@@ -1,5 +1,9 @@
 from loguru import logger
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from models.user import User
+
 from aiogram import Router, types
 from aiogram.filters import CommandStart, Command
 
@@ -8,9 +12,30 @@ command_router = Router(name='Bot commands')
 
 
 @command_router.message(CommandStart())
-async def start(message: types.Message):
+async def start(message: types.Message, session: AsyncSession):
     logger.info(f'User {message.from_user.username} send command {message.text}')
     answer_message = f'Привет, {message.from_user.first_name} {message.from_user.last_name}\nПолучается это бот 🦆'
+
+    result = await session.execute(select(User).filter_by(tg_id=message.from_user.id))
+    user = result.scalars().first()
+
+    if user:
+        user.username = message.from_user.username
+        user.first_name = message.from_user.first_name
+        user.last_name = message.from_user.last_name
+        await session.commit()
+        logger.info(f'User {user.username} was updated in the database')
+    else:
+        new_user = User(
+            username=message.from_user.username,
+            tg_id=message.from_user.id,
+            first_name=message.from_user.first_name,
+            last_name=message.from_user.last_name
+        )
+        session.add(new_user)
+        await session.commit()
+        logger.info(f'User {user.username} add in the database')
+
     await message.answer(answer_message)
     logger.info(f'Bot send to user message: {answer_message}')
 
